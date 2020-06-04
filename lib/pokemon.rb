52779637146d0e4afb87fa4bc1472f55fb9ae579
Steve Anderson <prototype909@msn.com>
2020-06-04 20:35:01 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
 class Pokemon
+
+  @@all = []
+
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialized(id, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @all << self
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+    INSERT INTO pokemon (name, type)
+    VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.type)
+    @id = DB[:conn].execute("SELECT * FROM pokemon")[0][0]
+  end
+
+  def self.Find(name, type)
+    sql = "SELECT * FROM pokemon WHERE name = ?"
+    result = DB[:conn].execute(sql, name, type)[0]
+    Pokemon.new(result[0], result[1], result[2])
+  end
 end
