92eb19fc1bcbb648af475923ce10ad4d8dc7490a
Steve Anderson <prototype909@msn.com>
2020-06-04 20:40:37 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,25 +1,4 @@
 class Pokemon
 
-  def initialized(id:, name:, type:, db:)
-    @id = id
-    @name = name
-    @type = type
-    @db = db
-    @all << self
-  end
 
-  def self.save(name, type, db)
-    sql = <<-SQL
-    INSERT INTO pokemon (name, type)
-    VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.type)
-    @id = DB[:conn].execute("SELECT * FROM pokemon")[0][0]
-  end
-
-  def self.Find(name, type)
-    sql = "SELECT * FROM pokemon WHERE name = ?"
-    result = DB[:conn].execute(sql, name, type)[0]
-    Pokemon.new(result[0], result[1], result[2])
-  end
 end
